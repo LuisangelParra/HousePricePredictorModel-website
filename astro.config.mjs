8baf3fe978a26627bad5b2293a55646638befe27
Luisangel Parra <lfaria@uninorte.edu.co>
2024-03-27 15:10:32 -0500

    Actualiza .gitignore y astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,25 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+const SERVER_PORT = 3000;
+
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+
+const LIVE_URL = "https://luisangelparra.github.io";
+
+const SCRIPT = process.env.npm_lifecycle_script || "";
+
+const isBuild = SCRIPT.includes("astro build");
+
+let BASE_URL = LOCALHOST_URL;
+
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: BASE_URL,
+  base: "/HousePricePredictorModel-website",
   integrations: [tailwind(), mdx(), sitemap()],
 });
